f6279146d122771baa0f70bbb12acecdf0dd83be
Ben Hanrahan <banrahan@ist.psu.edu>
2017-10-17 15:42:40 -0400

    got a basic overlay working, make it a bit better...but here is basically how to do it

--- content.js
+++ content.js
@@ -4,3 +4,13 @@
 // A js file that runs in the context of webpages
 // Runs immediately when you visit a page allow in the manifest
 // under content_scripts > matches
+
+
+// BVH: This is a bit of a hack
+var sheet = document.styleSheets[0];
+sheet.insertRule('.dimmed:after {content: " "; z-index: 10; display: block; position: absolute; height: 100%; top: 0; left: 0; right: 0; background: rgba(0, 0, 0, 0.8);}', sheet.cssRules.length);
+sheet.insertRule('.dimmed {position: relative;}', sheet.cssRules.length);
+
+// BVH: this works but you will probably need to reload it, since facebook does infinite scrolling
+var posts = document.getElementsByClassName("_5jmm _5pat _3lb4 o_9kam8ma7g");
+posts[0].className += ' dimmed';
