9f7fa6190b608cdaa0ff7584aaf99db710956ab4
Ben Hanrahan <banrahan@ist.psu.edu>
2017-10-23 15:38:07 -0400

    clean up the commit that this -> guy did

--- background.js
+++ background.js
@@ -6,3 +6,15 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.create({"url": "https://github.com/n1kk0s/ChromeExtension"});
 });
+
+// when the extension is first installed
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.storage.sync.set({clean_news_feed: true});
+});
+
+// listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+    if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+});
