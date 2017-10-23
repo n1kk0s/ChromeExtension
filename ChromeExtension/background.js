d1f22c3a42cc0b22e37d38c11c92edc8ef2479cc
Wesley Mauk <WesleyM77@users.noreply.github.com>
2017-10-22 20:28:08 -0400

    Got a base working
    works on "the onion" articles on facebook only for right now.

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,1 +1,20 @@
 // background.js
+// Does stuff in the background. Has access to APIs.
+// A Single long-running script to manage a task or state
+
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({"url": "https://github.com/n1kk0s/ChromeExtension"});
+});
+
+// when the extension is first installed
+chrome.runtime.onInstalled.addListener(function(details) {
+    chrome.storage.sync.set({clean_news_feed: true});
+});
+
+// listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(function(id, info, tab){
+    if (tab.url.toLowerCase().indexOf("facebook.com") > -1){
+        chrome.pageAction.show(tab.id);
+    }
+});
