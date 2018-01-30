d20c954661787422f4ef034382aa3d53f6c29268
Ben Hanrahan <banrahan@ist.psu.edu>
2018-01-30 17:31:23 -0500

    fixed removing the overlay

--- content.js
+++ content.js
@@ -2,11 +2,12 @@
 
 var storyContainerClasses = ["_5jmm", "_5pcr"];
 
+// insert the 'dimmed' css rule into the stylesheet
 var sheet = document.styleSheets[0];
 sheet.insertRule('.dimmed:after {content: " "; z-index: 10; display: block; position: absolute; height: 100%; top: 0; left: 0; right: 0; background: rgba(255, 255, 255, 0.75); }', sheet.cssRules.length);
 sheet.insertRule('.dimmed {position: relative;}', sheet.cssRules.length);
 // overlay style sheet rules
-sheet.insertRule('#overlayDiv {position: absolute; z-index: 11; height: 100%; top: 0; left: 0; right: 0; -webkit-backdrop-filter: blur(10px); backdrop-filter: blur(10px);}', sheet.cssRules.length);
+sheet.insertRule('.overlayDiv {position: absolute; z-index: 11; height: 100%; top: 0; left: 0; right: 0; -webkit-backdrop-filter: blur(10px); backdrop-filter: blur(10px);}', sheet.cssRules.length);
 
 var style = document.createElement('link');
 style.rel = 'stylesheet';
@@ -27,44 +28,55 @@ var bannedReasons = ["Satire", "Conspiracy", "Incorrect", "Fake",
 function cleanNewsFeed(){
     chrome.storage.sync.get("clean_news_feed", function(data){
         if (data["clean_news_feed"]){
-            // find all potential posts
+
+            // find all potential story posts
             _.each(storyContainerClasses, function(storyContainerClass){
+                // filter the posts
                 posts = document.getElementsByClassName(storyContainerClass);
+                // loop through all of the story posts
                 _.each(posts, function(post){
                     checkLinks(post);
                 });
             });
+
         }
     });
 }
 
-
 function checkLinks(item){
     var links = item.getElementsByTagName("a");
-    _.each(links, function(link){
+
+    _.each(links, function(link) {
         var href = link.href.toLowerCase();
         var i = 0;
-        _.each(bannedDomains, function(domain){
-          var itemClassName = item.className.toString();
-          console.log(href);
-            if (href.indexOf(domain.toLowerCase()) != -1 && (itemClassName.indexOf('dimmed') == -1)){
-                // edit the post here
-                editedPosts[editedPosts.length] = (item.className += ' dimmed');
-                // create an unique overlay with a link to the actual post
-                var div = document.createElement( 'div' );
-                // USE IDS CORRECTLY
-                div.id = 'overlayDiv';
 
-                // edit the string here to edit the overlay div HTML
-                div.innerHTML = '<center><h1 class="overlay">This article was flagged as ' + bannedReasons[i].toLowerCase()
-                + '. It may be unreliable.</h1><h4 class="overlay">To read, click <a target="blank" href="#" onclick="function();"> here. </a> </h4></center>';
-                // This is where we add the div.
-                item.appendChild(div);
+        _.each(bannedDomains, function(domain) {
+            var itemClassName = item.className.toString();
+            console.log(href);
 
-                div.onclick = function() {
-                    // HERE IS WHERE YOU DO THE STUFF
-                    // REMOVE DIMMED CLASS FROM DIV
-                };
+            if (href.indexOf(domain.toLowerCase()) != -1 && (itemClassName.indexOf('dimmed') == -1)) {
+
+                // keep track of edited posts here
+                if (!editedPosts.includes(item.id)) {
+                    editedPosts.push(item.id);
+                    item.className += ' dimmed';
+                    // create an unique overlay with a link to the actual post
+                    var div = document.createElement( 'div' );
+                    div.className = 'overlayDiv';
+
+                    // edit the string here to edit the overlay div HTML
+                    div.innerHTML = '<center><h1 class="overlay">This article was flagged as ' + bannedReasons[i].toLowerCase() + '. It may be unreliable.</h1><h4 class="overlay">To read, click <a target="blank" href="#" onclick="function();"> here. </a> </h4></center>';
+                    // This is where we add the div.
+                    item.appendChild(div);
+
+                    div.onclick = function() {
+                        var _div_ = div;
+                        _div_.remove();
+
+                        var _parent_ = item;
+                        _parent_.classList.remove("dimmed");
+                    };
+                }
             }
           i = i + 1;
         });
